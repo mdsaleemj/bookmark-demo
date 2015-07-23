d442ce8aba021c14376e94edc4ca9a9780b9ac14
mdsaleemj <mdsaleemj@gmail.com>
2015-07-23 23:16:38 +0530

    rest call testing

---
+++ test/rest_request.sh
@@ -0,0 +1,9 @@
+#to add new bookmark
+curl -i -H "Content-Type: application/json" -d '{"title": "google" ,"url" : "www.google.com"}'  http://localhost:20020/api/bookmark
+
+#To delete a bookmark 
+curl -i  -X DELETE http://localhost:20020/api/bookmark/{$id}
+
+#To get the list of bookmark
+
+culr -i http://localhost:20020/api/bookmark
